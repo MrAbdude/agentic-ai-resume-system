f629a231dc305419c8a9877fd00088a9a0657b24
Abdulwaood <142042288+MrAbdude@users.noreply.github.com>
2026-03-10 05:06:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://agentic-ai-resume-system.netlify.app'
+    'https://agentic-resumesystem.netlify.app'
   ]
 }))
 app.use(express.json())
